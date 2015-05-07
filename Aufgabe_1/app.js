51ff11e212fc272445a4260d5810ac8103d6fc59
rseibt <romanseibt@gmail.com>
2015-05-07 20:10:19 +0200

    console.log

--- Aufgabe_1/app.js
+++ Aufgabe_1/app.js
@@ -5,7 +5,10 @@ fs.readFile(__dirname+"/wolkenkratzer.json", function(err, data) {
 		if (err) throw err;
 
 			var emulated = JSON.parse(data.toString());
-		}
 
-		
+		for(var i in emulated.wolkenkratzer){
+			console.log(emulated.wolkenkratzer[i].name);
+			console.log(emulated.wolkenkratzer[i].stadt);
+			console.log(emulated.wolkenkratzer[i].hoehe);
+		}
  });
