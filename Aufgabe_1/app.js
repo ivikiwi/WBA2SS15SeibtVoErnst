3f9f5169df9033266bfc098cd21b3e3e649a4be8
Markusernst <markusernst92@web.de>
2015-05-07 21:16:19 +0200

    writeFile
    writeFile sorted Wolkenkratzer

--- Aufgabe_1/app.js
+++ Aufgabe_1/app.js
@@ -20,10 +20,15 @@ fs.readFile(__dirname+"/wolkenkratzer.json", function(err, data) {
 				}
 			);
 
-		for(var i in emulated.wolkenkratzer){
-			console.log(chalk.green("Name:" + emulated.wolkenkratzer[i].name));
-			console.log(chalk.yellow("Stadt:" + emulated.wolkenkratzer[i].stadt));
-			console.log(chalk.red("Hoehe:" + emulated.wolkenkratzer[i].hoehe));
-			console.log("--------------------------------------");
-		}
+		fs.writeFile(__dirname+"/sorted_wolkenkratzer.json", JSON.stringify(emulated), function(err) {
+
+			if (err) throw err;
+
+			for(var i in emulated.wolkenkratzer){
+				console.log(chalk.green("Name:" + emulated.wolkenkratzer[i].name));
+				console.log(chalk.yellow("Stadt:" + emulated.wolkenkratzer[i].stadt));
+				console.log(chalk.red("Hoehe:" + emulated.wolkenkratzer[i].hoehe));
+				console.log("--------------------------------------");
+			}
+		});
  });
