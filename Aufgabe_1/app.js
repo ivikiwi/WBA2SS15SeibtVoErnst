659afdbdbee1999b6d94c757ac123dc174c97b84
Markusernst <markusernst92@web.de>
2015-05-07 21:07:44 +0200

    sort
    sort wolkenkratzer

--- Aufgabe_1/app.js
+++ Aufgabe_1/app.js
@@ -7,10 +7,23 @@ fs.readFile(__dirname+"/wolkenkratzer.json", function(err, data) {
 
 			var emulated = JSON.parse(data.toString());
 
+			emulated.wolkenkratzer.sort(function(a,b) {
+
+				if (a.hoehe > b.hoehe) {
+   					return 1;
+  				}
+  				if (a.hoehe < b.hoehe) {
+    				return -1;
+  				}
+  				// a must be equal to b
+  					return 0;
+				}
+			);
+
 		for(var i in emulated.wolkenkratzer){
 			console.log(chalk.green("Name:" + emulated.wolkenkratzer[i].name));
 			console.log(chalk.yellow("Stadt:" + emulated.wolkenkratzer[i].stadt));
 			console.log(chalk.red("Hoehe:" + emulated.wolkenkratzer[i].hoehe));
-			console.log("--------------------------------------")
+			console.log("--------------------------------------");
 		}
  });
