b49f165eda25c486233c280e19f67a405dfcccfd
rseibt <romanseibt@gmail.com>
2015-05-07 19:54:08 +0200

    JSON.parse

--- Aufgabe_1/app.js
+++ Aufgabe_1/app.js
@@ -3,4 +3,9 @@ var fs = require('fs');
 fs.readFile(__dirname+"/wolkenkratzer.json", function(err, data) { 
 
 		if (err) throw err;
+
+			var emulated = JSON.parse(data.toString());
+		}
+
+		
  });
