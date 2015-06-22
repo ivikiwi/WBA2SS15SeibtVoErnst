d9cc5a2903fe62817d64bdc8bab62517f89893b3
Markusernst <markusernst92@web.de>
2015-06-22 09:43:09 +0200

    get at user on id

--- Projekt/client/app.js
+++ Projekt/client/app.js
@@ -71,4 +71,47 @@ app.get('/cover/:id', function (req, res, next) {
 
 })
 
+
+app.get('/allseries/:id', jsonParser, function(req, res){
+	fs.readFile('./series.ejs', {encoding: 'utf-8'}, function(err, filestring) {
+		if(err) {
+			throw err;
+		} else {
+			var options = {
+				host: 'localhost',
+				port: 8888,
+				path: '/series/:id',
+				method: 'GET',
+				headers: {
+					accept: 'application/json'
+				}
+			}
+			var externalRequest = http.request(options, function(externalRequest) {
+				console.log('Connected');
+				externalRequest.on('data', function(chunk) {
+
+					var seriesdata = JSON.parse(chunk);
+
+					var html = ejs.render(filestring, seriesdata);
+					res.setHeader('content-type', 'text/html');
+					res.writeHead(200);
+					res.write(html);
+					res.end();
+
+				});
+
+			});
+
+			externalRequest.end();
+		}
+	});
+});
+
+
+
+
+
+
+
+
 app.listen(3001);
