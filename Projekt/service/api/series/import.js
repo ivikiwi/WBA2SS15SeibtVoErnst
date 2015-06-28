1383502ad3bfc438f76b7f691b54caa9cdc9a04f
Ivanka <ivanka.vo@googlemail.com>
2015-06-28 18:43:55 +0200

    added app to import routes

--- Projekt/service/api/series/import.js
+++ Projekt/service/api/series/import.js
@@ -1,16 +1,16 @@
 function register(app, db) {
-	app.post("/series", require("./post-series")(db));
-	app.get("/series/:id", require("./get-series")(db));
-	app.put("/series/:id", require("./change-series")(db));
-	app.delete("/series/:id", require("./delete-series")(db));
-	app.get("/series", require("./get-all-series")(db));
-	app.get("/series/name/:id", require("./get-series-name")(db));
-	app.get("/series/description/:id", require("./get-series-description")(db));
-	app.post("/series/:sid/season", require("./post-season")(db));
-	app.get("/series/:sid/season/:id", require("./get-season")(db));
-	app.put("/series/:sid/season/:id", require("./change-season")(db));
-	app.delete("/series/:sid/season/:id", require("./delete-season")(db));
-	app.get("/series/:sid/season", require("./get-all-seasons")(db));
+	app.post("/series", require("./post-series")(app, db));
+	app.get("/series/:id", require("./get-series")(app, db));
+	app.put("/series/:id", require("./change-series")(app, db));
+	app.delete("/series/:id", require("./delete-series")(app, db));
+	app.get("/series", require("./get-all-series")(app, db));
+	app.get("/series/name/:id", require("./get-series-name")(app, db));
+	app.get("/series/description/:id", require("./get-series-description")(app, db));
+	app.post("/series/:sid/season", require("./post-season")(app, db));
+	app.get("/series/:sid/season/:id", require("./get-season")(app, db));
+	app.put("/series/:sid/season/:id", require("./change-season")(app, db));
+	app.delete("/series/:sid/season/:id", require("./delete-season")(app, db));
+	app.get("/series/:sid/season", require("./get-all-seasons")(app, db));
 }
 module.exports = {
 	register: register
