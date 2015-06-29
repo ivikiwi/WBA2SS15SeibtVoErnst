84213647def6ee2a0cf88fcf4b827ffbdda5e6cd
romanseibt <romanseibt@gmail.com>
2015-06-29 11:01:57 +0200

    Getseasons in getseries
    Ergänzungen

--- Projekt/service/api/series/get-series.js
+++ Projekt/service/api/series/get-series.js
@@ -1,14 +1,20 @@
 function getSeries(app, db) {
 	return function(req, res) {
 		
-		db.get("series:" + req.params.id, function(err, rep) {
-			if (rep) {
-				res.type("json").send(rep);
+		db.get("series:" + req.params.id, function(err, seriesDataRep) {
+			var seriesData = seriesDataRep;
+			db.get(req.params.id+":sssincr", function(error, seasonsCountRep){
+					var seasonsCount = seasonsCountRep;
+					console.log(seasonCount);
+
+			if(seasonsCountRep) {
+				res.type("json").send({
+					seriesdata: JSON.parse(seriesData),
+					seasons: seasonsCount
+				});
 			}
-			else {
-				res.status(404).type("text").send("Die Serie mit der ID " + req.params.id + " wurde nicht gefunden");
-			}
+			});
 		});
-	};
-}
+	}
+			
 module.exports = getSeries;
