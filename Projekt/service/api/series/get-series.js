11d93a181f2f69ba996c378cc49cedbd330a5bd9
romanseibt <romanseibt@gmail.com>
2015-06-29 11:12:46 +0200

    more brackets - fuxed
    asasd

--- Projekt/service/api/series/get-series.js
+++ Projekt/service/api/series/get-series.js
@@ -3,19 +3,22 @@ function getSeries(app, db) {
 		
 		db.get("series:" + req.params.id, function(err, seriesDataRep) {
 			var seriesData = seriesDataRep;
-			db.get(req.params.id+":sssincr", function(error, seasonsCountRep){
-					var seasonsCount = seasonsCountRep;
-					console.log(seasonCount);
-
-			if(seasonsCountRep) {
+			db.get(req.params.id+":sssincr", function(error, seasonsCountRep ) {
+				var seasonsCount = seasonsCountRep;
+				console.log(seasonsCount);
+			if (seasonsCountRep) {
 				res.type("json").send({
 					seriesdata: JSON.parse(seriesData),
 					seasons: seasonsCount
 				});
 			}
+			else {
+			//	res.status(404).type("text").send("Die Serie mit der ID " + req.params.id + " wurde nicht gefunden");
+			}
+
+
 			});
 		});
-	}
+	};
 }
-			
 module.exports = getSeries;
