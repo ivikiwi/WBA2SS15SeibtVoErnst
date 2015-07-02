4ae459b5d433b864f4c77a5e4e7c0fbc044434dd
Ivanka <ivanka.vo@googlemail.com>
2015-07-02 19:54:49 +0200

    updated get series to try getting episodes data

--- Projekt/service/api/series/get-series.js
+++ Projekt/service/api/series/get-series.js
@@ -6,15 +6,24 @@ function getSeries(app, db) {
 			db.get(req.params.id+":sssincr", function(error, seasonsCountRep ) {
 				var seasonsCount = seasonsCountRep;
 				console.log(seasonsCount);
-			if (seasonsCountRep) {
-				res.type("json").send({
-					seriesdata: JSON.parse(seriesData),
-					seasons: seasonsCount
-				});
-			}
-			else {
-			//	res.status(404).type("text").send("Die Serie mit der ID " + req.params.id + " wurde nicht gefunden");
-			}
+				var episodeCounts = [];
+				for(var i=0; i < seasonsCount; i++) {
+					db.get("ss:"+req.params.id+":season:"+i, function(episodeCounterr, episodeCountRep) {
+							episodeCounts.push(JSON.parse(episodeCountRep));
+						if (seasonsCountRep) {
+							res.type("json").send({
+								episodes: episodeCounts,
+								seriesdata: JSON.parse(seriesData),
+								seasons: seasonsCount
+							});
+						}
+						else {
+				//res.status(404).type("text").send("Die Serie mit der ID " + req.params.id + " wurde nicht gefunden");
+						}
+					});
+				}
+
+			
 
 
 			});
