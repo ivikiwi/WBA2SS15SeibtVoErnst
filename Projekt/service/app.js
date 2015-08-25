c27157de1e961da8ee946f11c5990446e1f61a24
Ivanka <ivanka.vo@googlemail.com>
2015-08-25 03:05:30 +0200

    deleted flushpostallroute

--- Projekt/service/app.js
+++ Projekt/service/app.js
@@ -13,8 +13,5 @@ app.use(jsonParser);
 require("./api/series/import").register(app, db);
 /*Register User API */
 require("./api/user/import").register(app, db);
-//	//////////////////////////////////////////
-/*Register Flush API*/
-require("./api/flushpostall/import").register(app, db);
 //	Server Port
 app.listen(8888);
